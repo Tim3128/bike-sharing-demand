75eea45ed3bd28484d450cc633ce541834737d93
Tim3128 <timothejannetta@hotmail.fr>
2019-03-29 17:58:02 +0100

    data_preprocessing

---
+++ data_preprocessing.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+dico_seasons = {1: 'Winter', 2: 'Spring', 3: 'Summer', 4: 'Fall'}
+
+def process(df_raw : pd.DataFrame):
+
+    df_data = df_raw.copy()
+
+    df_data['datetime'] = pd.to_datetime(df_data["datetime"])
+    df_data['hour'] = df_data['datetime'].apply(lambda d: d.hour)
+
+    df_data['season'] = df_data['season'].apply(lambda s: dico_seasons[s])
+
+    days = df_data['datetime'].apply(lambda d: d.day_name())
+    df_data['working_day'] = 1-days.isin(['Saturday', 'Sunday'])
+
+    df_data.set_index('datetime', inplace=True)
+
+    df_data[['hour', 'weather', 'season']] = df_data[['hour', 'weather', 'season']].astype('category')
+
+    df_data = pd.get_dummies(df_data)
+
+    return df_data
+
+# if __name__ == '__main__':
+#
+#     df_test = pd.read_csv('test.csv')
+#
+#     df_test = process(df_test)
+#
+#     pass
