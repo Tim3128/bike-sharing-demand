c7052e845a7ad739c6b303c42997bce7da16c622
Tim3128 <timothejannetta@hotmail.fr>
2019-04-10 16:26:50 +0200

    new submissions

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -8,22 +8,16 @@ def process(df_raw : pd.DataFrame):
 
     df_data['datetime'] = pd.to_datetime(df_data["datetime"])
     df_data['hour'] = df_data['datetime'].apply(lambda d: d.hour)
+    #df_data['day']=df_data['datetime'].apply(lambda d : d.day)
+    #df_data['month']=df_data['datetime'].apply(lambda d : d.month)
+    #df_data['year']=df_data['datetime'].apply(lambda d : d.year)
 
     df_data['season'] = df_data['season'].apply(lambda s: dico_seasons[s])
 
     df_data.set_index('datetime', inplace=True)
 
-    #df_data[['hour', 'weather', 'season']] = df_data[['hour', 'weather', 'season']].astype('category')
     df_data[['weather', 'season']] = df_data[['weather', 'season']].astype('category')
 
     df_data = pd.get_dummies(df_data)
 
     return df_data
-
-# if __name__ == '__main__':
-#
-#     df_test = pd.read_csv('test.csv')
-#
-#     df_test = process(df_test)
-#
-#     pass
